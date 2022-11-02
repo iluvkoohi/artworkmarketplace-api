c3533ee3a19321ebeb4a1b0bb06cfdf2bb5a3598
Kolya Madridano <58349866+iluvkoohi@users.noreply.github.com>
2022-11-02 09:20:56 +0800

    Fix(routeTicket): Remove protected route

--- routes/routeTicket.js
+++ routes/routeTicket.js
@@ -7,24 +7,19 @@ const ticket = require("../controllers/ticket/verificationController");
 
 // VERIFICATION
 router.post("/ticket/verification",
-    isAuthenticated,
     getProfile,
     (req, res) => ticket.create(req, res));
 
 router.get("/ticket/verification",
-    isAuthenticated,
     (req, res) => ticket.all(req, res));
 
 router.get("/ticket/verification/s/:id",
-    isAuthenticated,
     (req, res) => ticket.getByAccountId(req, res));
 
 router.put("/ticket/verification",
-    isAuthenticated,
     (req, res) => ticket.updateProfileVerificationStatus(req, res));
 
 router.delete("/ticket/verification/:id",
-    isAuthenticated,
     (req, res) => ticket.deleteVerificationTicket(req, res));
 
 module.exports = router;
